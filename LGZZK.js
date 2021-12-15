49c78619cdb8e4edb444419aff5e481adad82b19
lgzzk <zzk171@outlook.com>
2021-12-15 19:40:07 +0800

    lgzzk 提交了修改

--- LGZZK.js
+++ LGZZK.js
@@ -255,7 +255,7 @@
 		}
 		xhr.onreadystatechange = function (){
 			if( xhr.readyState === 4 ){
-				if( xhr.status >= 200 && xhr.status < 300 || xhr.status == 304 ){
+				if( xhr.status >= 200 && xhr.status < 300 || xhr.status === 304 ){
 					if( dataType === "text" ){
 						res = xhr.responseText;
 					}else if( dataType === "xml" ){
